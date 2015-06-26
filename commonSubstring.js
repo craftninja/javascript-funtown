e0c29db219b21cec4b5706acd3396088f6fb40bd
EmilyPlatzer <emilyplatzer@gmail.com>
2015-06-26 10:46:36 -0600

    Common substring

---
+++ commonSubstring.js
@@ -0,0 +1,37 @@
+// Given two strings, write a function that returns the longest common substring.
+
+// lcs('havoc', average) => 'av'
+// lcs('baddd', 'dddabc') => 'ddd'
+// lcs('food', 'lame') => ''
+// lcs('abcddd', 'dddabc') => 'abc'
+
+function lcs(stringA, stringB) {
+  var startA, startB, common;
+  var answers = [];
+  for (var i = 0; i < stringA.length; i++) {
+    common = '';
+    startA = i;
+    startB = stringB.indexOf(stringA[i]);
+    if (startB >=0) {
+      var it = 0;
+      while (stringA[startA+it] == stringB[startB+it]) {
+        common += stringA[startA+it];
+        it ++;
+      };
+      answers.push(common);
+    };
+  };
+  if (answers.length > 0) {
+    var answer = answers.reduce(function(prevVal, currVal) {
+      return prevVal.length >= currVal.length ? prevVal : currVal;
+    });
+  } else {
+    answer = '';
+  };
+  return answer;
+};
+
+console.log(lcs('havoc', 'average'));
+console.log(lcs('baddd', 'dddabc'));
+console.log(lcs('food', 'lame'));
+console.log(lcs('abcddd', 'dddabc'));
