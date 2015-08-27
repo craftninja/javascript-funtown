7d7b6fd41ed7ad85f0756e3c527e337550e5ebbf
EmilyPlatzer <emilyplatzer@gmail.com>
2015-08-27 10:50:00 -0600

    User can convert any base number into decimal

--- toDecimal.js
+++ toDecimal.js
@@ -1,19 +1,21 @@
-function toDecimal(binary) {
+function toDecimal(number, base) {
   var decimal = 0;
-  var revBinaryList = String(binary).split('').reverse();
-  revBinaryList.forEach(function(bit, index) {
-    decimal += Number(bit) * Math.pow(2, index);
+  var revNumberList = String(number).split('').reverse();
+  revNumberList.forEach(function(bit, index) {
+    decimal += Number(bit) * Math.pow(base, index);
   });
   return decimal;
 };
 
 console.log('expect 0');
-console.log('actual', toDecimal(0)); // 0
+console.log('actual', toDecimal(0, 2)); // 0
 console.log('expect 1');
-console.log('actual', toDecimal(1)); // 1
+console.log('actual', toDecimal(1, 2)); // 1
 console.log('expect 3');
-console.log('actual', toDecimal(11)); // 3
+console.log('actual', toDecimal(11, 2)); // 3
 console.log('expect 8');
-console.log('actual', toDecimal(1000)); // 8
+console.log('actual', toDecimal(1000, 2)); // 8
 console.log('expect 12');
-console.log('actual', toDecimal(1100)); // 12
+console.log('actual', toDecimal(1100, 2)); // 12
+console.log('expect 177');
+console.log('actual', toDecimal(2301, 4));
